907a79cc96b18445e4707ca684c8b764a0db5a3d
Makhambet <krutchdd@gmail.com>
2022-05-02 11:56:12 +0600

    port fixed

--- dial/main.go
+++ dial/main.go
@@ -9,26 +9,26 @@ import (
 
 func main() {
 
-	// Подключаемся к сокету
-	conn, err := net.Dial("tcp", "127.0.0.1:8081")
+	conn, err := net.Dial("tcp", "127.0.0.1:9000")
 	if err != nil {
 		panic(err)
 	}
 	for {
-		// Чтение входных данных от stdin
 		reader := bufio.NewReader(os.Stdin)
+
 		fmt.Print("Enter your name: ")
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
-		// Отправляем в socket
+
 		fmt.Fprintf(conn, text+"\n")
-		// Прослушиваем ответ
+
 		message, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
+
 		fmt.Print("Hello, " + message)
 	}
 }
--- listen/main.go
+++ listen/main.go
@@ -12,31 +12,25 @@ func main() {
 
 	fmt.Println("Launching server...")
 
-	// Устанавливаем прослушивание порта
-	ln, err := net.Listen("tcp", ":8081")
+	ln, err := net.Listen("tcp", ":9000")
 
-	if err != nil { 
+	if err != nil {
 		panic(err)
 	}
-	// Открываем порт
 	conn, err := ln.Accept()
 
-	if err != nil { 
+	if err != nil {
 		panic(err)
 	}
 
-	// Запускаем цикл
 	for {
-		// Будем прослушивать все сообщения разделенные \n
 		message, err := bufio.NewReader(conn).ReadString('\n')
-		
+
 		if err != nil {
 			break
 		}
-		// Распечатываем полученое сообщение
 		fmt.Print("Message Received:", string(message))
 
-		// Отправить новую строку обратно клиенту
 		conn.Write([]byte(message + "\n"))
 	}
 }
